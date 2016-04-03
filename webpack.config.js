aa67cf371a75bf88760fce2dd1da9095d37b1d42
S K <xz64@users.noreply.github.com>
2016-04-03 15:43:44 -0400

    add title and minification to html

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
+var isProduction = process.env.NODE_ENV === 'production';
+
 var config = {
   entry: path.join(__dirname, './index.js'),
   output: {
     path: 'dist',
     filename: 'app.js'
   },
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Voting App',
+    minify: {
+      collapseWhitespace: true
+    }
+  })]
 };
 
 module.exports = config;
