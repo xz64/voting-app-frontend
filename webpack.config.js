6ef16d0e410cbeeb511d73a192d43362cfbb9dac
S K <xz64@users.noreply.github.com>
2016-04-03 15:48:28 -0400

    add clean webpack plugin
    This will empty out the dist directory before building.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 var path = require('path');
 
+var buildDir = 'dist';
 var isProduction = process.env.NODE_ENV === 'production';
 
 var config = {
   entry: path.join(__dirname, 'index.js'),
   output: {
-    path: 'dist',
+    path: buildDir,
     filename: 'app.js'
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'Voting App',
-    minify: {
-      collapseWhitespace: true
-    }
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Voting App',
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    new CleanWebpackPlugin([buildDir])
+  ]
 };
 
 module.exports = config;
