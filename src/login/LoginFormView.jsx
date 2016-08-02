51102bd8e90bc52c091ee3437b90138f9141a04f
S K <xz64@users.noreply.github.com>
2016-08-02 03:53:54 +0000

    feat: enhance login styles

--- src/login/LoginFormView.jsx
+++ src/login/LoginFormView.jsx
@@ -7,17 +7,18 @@ import styles from './LoginFormView.css';
 
 const LoginFormView = ({ fields: { email, password } , handleSubmit,
   submitting }) => (
-  <div>
+  <div styleName="loginform">
     <form onSubmit={handleSubmit}>
-      <InputText formField={email} placeholder='Your email address' 
-        forAttr='email' labelText='Email Address' type='text'/>
-      <InputText formField={password} placeholder='*******' 
-        forAttr='password' labelText='Password' type='password'/>
+      <InputText formField={email} forAttr='email' labelText='Email Address'
+        type='text'/>
+      <InputText formField={password} forAttr='password' labelText='Password'
+        type='password'/>
       <button styleName='submitbutton' type="submit" disabled={submitting}>
         {submitting ? 'Please wait...' : 'Login'}
       </button>
     </form>    
-    <Link to='register'>Register</Link>
+    Don't have a login? Click <Link styleName='link' to='register'>here</Link>
+    {' '}to register.
   </div>
 );
 
