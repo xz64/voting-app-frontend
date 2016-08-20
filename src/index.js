067ce2d2983f9286aa30ec899554a1a4c0f702a3
S K <xz64@users.noreply.github.com>
2016-08-20 20:24:38 +0000

    feat: add API call in register form submit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { showLoading } from 'react-redux-loading-bar';
 import 'blaze/dist/generic.global.min.css';
+import 'whatwg-fetch';
 
 import i18n from './i18n/i18n.js';
 import routes from './app/AppRoute.js';
--- src/register/RegisterContainer.jsx
+++ src/register/RegisterContainer.jsx
@@ -8,8 +8,23 @@ const RegisterContainer = reduxForm({
   form: 'register',
   fields: ['email', 'password', 'repeatpassword', 'captcharesponse'],
   validate,
-  onSubmit: (values) => new Promise((resolve/*, reject*/) => {
-    setTimeout(() => resolve(values), 1000);
+  onSubmit: (values) => new Promise((resolve, reject) => {
+    fetch('/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(values)
+    })
+    .then(function(data) {
+      return data.text();
+    })
+    .then(function() {
+      resolve(values);
+    })
+    .catch(function(err) {
+      reject(err);
+    });
   }),
   onSubmitSuccess: (/*values, dispatch*/) => {
     // notify user a confirmation email has been sent.
