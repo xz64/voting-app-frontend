0cf6b174c4cc14eb2afd0b7fb09273f69a899a24
S K <xz64@users.noreply.github.com>
2016-10-30 21:17:48 +0000

    fix: hide login link when on login page

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { connect } from 'cerebral-view-react';
 
 const stateProps = {
-  userID: 'app.user.id'
+  userID: 'app.user.id',
+  currentRoute: 'app.currentRoute'
 };
 
-const NavBar = ({ userID }) => (
+const NavBar = ({ userID, currentRoute }) => (
   <div role='presentation' className='va-navbar-spacer'>
     <nav className='va-navbar'>
       <a className='va-navlink' href='/#/'>Home</a>
       <a className='va-navlink' href='/#/about'>About</a>
       <div className='va-spacer' role='presentation'/>
       {userID ? (<a className='va-navlink' href='/#/'>{userID}</a>) : null}
-      {userID ? (<a className='va-navlink' href='/#/logout'>Logout</a>) :
-        (<a className='va-navlink' href='/#/login'>Login</a>)}
+      {userID ?
+          (<a className='va-navlink' href='/#/logout'>Logout</a>)
+          : ((currentRoute === 'login') ?
+            null
+            : (<a className='va-navlink' href='/#/login'>Login</a>))}
       {/* TODO: only show hamburger icon when logged in
       <button type='button' styleName='hamburger'>&#x2261;</button>*/}
     </nav>
@@ -21,7 +25,8 @@ const NavBar = ({ userID }) => (
 );
 
 NavBar.propTypes = {
-  userID: React.PropTypes.string
+  userID: React.PropTypes.string,
+  currentRoute: React.PropTypes.string
 };
 
 export default connect(stateProps, {}, NavBar);
